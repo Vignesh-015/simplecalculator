6e3264308a94b080cde11790a75e24bde29fd8a6
Vignesh-015 <130903978+Vignesh-015@users.noreply.github.com>
2024-03-05 23:31:03 +0530

    Add files via upload

---
+++ calci/script.js
@@ -0,0 +1,16 @@
+function appendNumber(number) {
+  document.getElementById('result').value += number;
+}
+
+function appendOperator(operator) {
+  document.getElementById('result').value += operator;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+  const result = eval(document.getElementById('result').value);
+  document.getElementById('result').value = result;
+}
